e9c6785205847d0b8ca47823a0c7deb82dc8ed9a
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-06-07 10:04:48 +0700

    add time log production

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'VNFITE_PUSH_NOTIFICATION',
+            script: './index.js',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        },
+    ],
+  };
